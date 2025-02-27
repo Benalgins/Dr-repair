e42da390ac8605572eea35f988fc85af234e9d9b
Benalgins <simeon.ivanew@gmail.com>
2025-02-27 17:40:08 +0200

    added best-seller and premium plan
    Both sections are added on the pricing plan page

--- src/components/pricing-plans.jsx
+++ src/components/pricing-plans.jsx
@@ -29,9 +29,24 @@ export default function PricingPlans() {
 									</li>
 									<li>
 										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
-										<p>Vehicle maintenance for annual check</p>
+										<p>Free Car Wash</p>
 									</li>
-									<li></li>
+									<li>
+										<img src="/Imgs/redcross.png" alt="" className="img-has" />
+										<p>Flat Tire Repair</p>
+									</li>
+									<li>
+										<img src="/Imgs/redcross.png" alt="" className="img-has" />
+										<p>Full Vehicle Diagnostic Check (Bi-Annually)</p>
+									</li>
+									<li>
+										<img src="/Imgs/redcross.png" alt="" className="img-has" />
+										<p>VIP Customer Support (Dedicated Hotline)</p>
+									</li>
+									<li>
+										<img src="/Imgs/redcross.png" alt="" className="img-has" />
+										<p>Discount on Spare Parts (10%)</p>
+									</li>
 								</ul>
 							</div>
 						</div>
@@ -40,20 +55,100 @@ export default function PricingPlans() {
 
 				<div className="best-offer plan">
 					<div className="plan-header">
-						<div className="plan-img"></div>
+						<div className="plan-img">
+							<div className="plan-img">
+								<img src="/Imgs/bestsellerimg.png" alt="" />
+							</div>
+						</div>
 						<div className="text">
 							<h3>Monthly:</h3>
 							<h2>225$</h2>
+							<div className="description">
+								<ul>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Tyre Change</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Oil Change</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Road Assistance (24/7) for Varna</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Free Car Wash</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Flat Tire Repair</p>
+									</li>
+									<li>
+										<img src="/Imgs/redcross.png" alt="" className="img-has" />
+										<p>Full Vehicle Diagnostic Check (Bi-Annually)</p>
+									</li>
+									<li>
+										<img src="/Imgs/redcross.png" alt="" className="img-has" />
+										<p>VIP Customer Support (Dedicated Hotline)</p>
+									</li>
+									<li>
+										<img src="/Imgs/redcross.png" alt="" className="img-has" />
+										<p>Discount on Spare Parts (10%)</p>
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
 
 				<div className="premium plan">
 					<div className="plan-header">
-						<div className="plan-img"></div>
+						<div className="plan-img">
+							<div className="plan-img">
+								<img src="/Imgs/premiumplanimg.png" alt="" />
+							</div>
+						</div>
 						<div className="text">
 							<h3>Monthly:</h3>
 							<h2>350$</h2>
+							<div className="description">
+								<ul>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Tyre Change</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Oil Change</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Unlimited Road Assistance (24/7)</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Free Car Wash</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Flat Tire Repair</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Full Vehicle Diagnostic Check (Bi-Annually)</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>VIP Customer Support (Dedicated Hotline)</p>
+									</li>
+									<li>
+										<img src="/Imgs/checkimg.png" alt="" className="img-has" />
+										<p>Discount on Spare Parts (10%)</p>
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
